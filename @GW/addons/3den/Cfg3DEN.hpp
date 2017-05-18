89c49d5ca83c2a554aa8b935dd33e366da820ecc
GuzzenVonLidl <guzzenvonlidlswe@gmail.com>
2017-05-18 20:10:49 +0200

    Version 0.4.1.180517

--- @GW/addons/3den/Cfg3DEN.hpp
+++ @GW/addons/3den/Cfg3DEN.hpp
@@ -72,13 +72,11 @@ class Cfg3DEN {
 
 	class EventHandlers {
 		class GW {
-//			OnMessage = "If ((_this select 0) isEqualTo 6) then {[5] call GW_3DEN_fnc_help};";
 			OnMessage = "if (((_this select 0) isEqualTo 6) && ((['GW_FRAMEWORK', 'Core', 'devBuild'] call BIS_fnc_getCfgData) isEqualTo -2)) then { ['Warning debug mode is enabled','I understand'] call BIS_fnc_3DENShowMessage; };";
-			onMissionLoad = "['load'] call GW_3DEN_fnc_init";
-			onMissionNew = "['new'] call GW_3DEN_fnc_init";
-			onMissionPreviewEnd = "['PreviewEnd'] call GW_3DEN_fnc_init";
-			OnMissionSave = "['save'] call GW_3DEN_fnc_init";
-			onTerrainNew = "call compile preProcessFileLineNumbers '\x\GW\addons\3den\XEH_preInit.sqf'; ['terrain'] call GW_3DEN_fnc_init";
+			onMissionLoad = "[] call GW_3DEN_fnc_init";
+			onMissionNew = "[] call GW_3DEN_fnc_init";
+			onMissionPreviewEnd = "[] call GW_3DEN_fnc_init";
+			onTerrainNew = "[] call GW_3DEN_fnc_init";
 		};
 	};
 
@@ -118,44 +116,3 @@ class Cfg3DEN {
 		};
 	};
 };
-
-
-
-/*
-			onConnectingEnd = "['connectEnd',_this] call bis_fnc_3DENControlsHint;";
-			onConnectingStart = "['connectStart',_this] call bis_fnc_3DENControlsHint;";
-			onEntityMenu = "['onEntityMenu',_this] call bis_fnc_3DENEntityMenu;";
-			onGridChange = "['OnGridChange',_this,true] call bis_fnc_3DENGrid;";
-			onHistoryChange = "['History'] call bis_fnc_3DENToolbar;";
-			onMapClosed = "['ToggleMap'] call bis_fnc_3DENToolbar;";
-			onMapOpened = "['ToggleMap'] call bis_fnc_3DENToolbar;";
-			onMessage = "_this call bis_fnc_3DENNotification;";
-			onMissionAutosave = "1 call bis_fnc_3DENNotification;";
-			onMissionListChange = "['onMissionListChange',0] call bis_fnc_3DENInterface;";
-			onMissionLoad = "['Init'] call bis_fnc_3DENToolbar; ['History'] call bis_fnc_3DENToolbar; ['MissionSection'] spawn bis_fnc_3DENToolbar; ['PlaceEmpty'] spawn bis_fnc_3DENInterface;	";
-			onMissionNew = "['History'] call bis_fnc_3DENToolbar; ['MissionSection'] spawn bis_fnc_3DENToolbar; ['PlaceEmpty'] spawn bis_fnc_3DENInterface; 0 call BIS_fnc_3DENVisionMode; 0 call BIS_fnc_3DENFlashlight;";
-			onMissionPreview = "['onMissionPreview',_this] spawn bis_fnc_3DENMissionPreview;";
-			onMissionPreviewEnd = "['onMissionPreviewEnd',_this] call bis_fnc_3DENMissionPreview; true call bis_fnc_3DENDrawLocations; _this'server' call bis_fnc_3DENStatusBar; [] spawn bis_fnc_3dentutorial; ['reset'] call bis_fnc_3DENEntityMenu; -2 spawn BIS_fnc_3DENVisionMode; -2 call BIS_fnc_3DENFlashlight; enableenvironment ('Preferences' get3DENMissionAttribute 'EnableEnvironment');";
-			onModeChange = "['showInterface',true] call bis_fnc_3DENInterface; ['showPanelRight',true] call bis_fnc_3DENInterface; ['mode'] spawn bis_fnc_3DENInterface; ['subMode'] spawn bis_fnc_3DENInterface;";
-			onMoveGridToggle = "['MoveGridToggle'] call bis_fnc_3DENToolbar;";
-			onRedo = "['History'] call bis_fnc_3DENToolbar;";
-			onRotateGridToggle = "['RotateGridToggle'] call bis_fnc_3DENToolbar;";
-			onScaleGridToggle = "['ScaleGridToggle'] call bis_fnc_3DENToolbar;";
-			onSearchCreate = "['TabRight',0] call bis_fnc_3DENInterface;";
-			onSearchEdit = "['TabLeft',0] call bis_fnc_3DENInterface;";
-			onSelectionChange = "'select' call bis_fnc_3DENControlsHint;";
-			onServerToggle = "'Server' call bis_fnc_3DENStatusBar; 'ButtonPlay' call bis_fnc_3DENInterface;";
-			onSubmodeChange = "['showInterface',true] call bis_fnc_3DENInterface; ['showPanelRight',true] call bis_fnc_3DENInterface; ['subMode'] spawn bis_fnc_3DENInterface;";
-			onSurfaceSnapToggle = "['SurfaceSnapToggle'] call bis_fnc_3DENToolbar;";
-			onTerrainNew = "true call bis_fnc_3DENDrawLocations;'init' call bis_fnc_3DENListLocations;'ButtonPlay' call bis_fnc_3DENInterface;'FadeIn' spawn bis_fnc_3DENInterface;['Init'] call bis_fnc_3DENToolbar;0 call BIS_fnc_3DENVisionMode;0 call BIS_fnc_3DENFlashlight;if (missionname == '' && ('preferences' get3denmissionattribute 'StartRandom')) then {'random' call bis_fnc_3DENCamera;};'onTerrainNew' call bis_fnc_3DENInterface;enableenvironment ('Preferences' get3DENMissionAttribute 'EnableEnvironment');";
-			onTogglePlaceEmptyVehicle = "['PlaceEmpty'] spawn bis_fnc_3DENInterface;";
-			onUndo = "['History'] call bis_fnc_3DENToolbar;";
-			onVerticalToggle = "['VerticalToggle'] call bis_fnc_3DENToolbar;";
-			onWidgetArea = "['WidgetToggle'] call bis_fnc_3DENToolbar;";
-			onWidgetNone = "['WidgetToggle'] call bis_fnc_3DENToolbar;";
-			onWidgetRotation = "['WidgetToggle'] call bis_fnc_3DENToolbar;";
-			onWidgetScale = "['WidgetToggle'] call bis_fnc_3DENToolbar;";
-			onWidgetToggle = "['WidgetToggle'] call bis_fnc_3DENToolbar;";
-			onWidgetTranslation = "['WidgetToggle'] call bis_fnc_3DENToolbar;";
-			onWorkspacePartSwitch = "['MissionSection'] spawn bis_fnc_3DENToolbar;";
-*/
--- @GW/addons/3den/functions/fnc_getCorrectVersion.sqf
+++ @GW/addons/3den/functions/fnc_getCorrectVersion.sqf
@@ -15,16 +15,16 @@
 
 params ["_copyGroup","_copyType"];
 
-if ((["GW_Modules", "Common", "version"] call BIS_fnc_getCfgData) < 1.5) then {
-	if (_copyGroup) then {
-		[_copyType] call FUNC(copyGroupOld);
-	} else {
-		[_copyType] call FUNC(copyStaticOld);
-	};
-} else {
+if ((["GW_Modules", "Common", "version"] call BIS_fnc_getCfgData) >= 1.5) then {
 	if (_copyGroup) then {
 		[_copyType] call FUNC(copyGroup);
 	} else {
 		[_copyType] call FUNC(copyStatic);
+	};
+} else {
+	if (_copyGroup) then {
+		[_copyType] call FUNC(copyGroupOld);
+	} else {
+		[_copyType] call FUNC(copyStaticOld);
 	};
 };
