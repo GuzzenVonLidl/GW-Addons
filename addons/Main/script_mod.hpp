ebc02130b1fbd20ffa708fb8f20c2822362edff4
GuzzenVonLidl <guzzenvonlidlswe@gmail.com>
2018-04-10 16:46:07 +0200

    0.7.1.100418

--- addons/Main/script_mod.hpp
+++ addons/Main/script_mod.hpp
@@ -2,7 +2,7 @@
 #define MAJOR 0
 #define MINOR 7
 #define PATCHLVL 1
-#define BUILD 160318	// We will use the DATE for the BUILD# in the format DDMMYY
+#define BUILD 100418	// We will use the DATE for the BUILD# in the format DDMMYY
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
