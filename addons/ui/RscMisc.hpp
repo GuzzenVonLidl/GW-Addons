f0bbd2a6769ac64f200daeb2f53c88487fe3d83b
GuzzenVonLidl <guzzenvonlidlswe@gmail.com>
2020-04-25 15:37:43 +0200

    Update RscMisc.hpp

--- addons/ui/RscMisc.hpp
+++ addons/ui/RscMisc.hpp
@@ -26,7 +26,6 @@ class GW_loadingScreen {
 		class Picture : RscPicture {
 			h = 1.0;
 			style = 48 + 0x800;
-//			text = "\x\gw\addons\3den\data\Logo_GOL_1.paa";
 			text = "\x\gw\addons\ui\data\Logo_Type_1.paa";
 			w = 1.0;
 			x = (safezoneX + safezoneW/2 - 0.5);
